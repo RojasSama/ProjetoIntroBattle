22fd1487ded04c3a3d7dc82f984fb94ae094d615
Luiz <luizfrojas@gmail.com>
2021-11-30 14:23:12 -0300

    organizando o codigo

--- programa/main.py
+++ programa/main.py
@@ -7,8 +7,14 @@ import sys
 pg.init()
 pg.font.init()
 
-bg_music = pg.mixer.music.load('musicas/BoxCat Games - Defeat.mp3')
-pg.mixer.music.play(-1)
+def draw_text(text, font, type, color, surface, x, y):
+    textObj = font.render(text, type, color)
+    textRect = textObj.get_rect()
+    textRect.topleft = (x, y)
+    surface.blit(textObj, textRect)
+    
+def menu():
+    draw_text('Main Menu', font, 1, (255, 255, 255), screen, 40, 30)
 
 screen_size = (1024, 768)
 screen = pg.display.set_mode((screen_size[0], screen_size[1]))
@@ -18,49 +24,20 @@ font = pg.font.SysFont(None, 50)
 
 clock = pg.time.Clock()
 
-def draw_text(text, font, type, color, surface, x, y):
-    textObj = font.render(text, type, color)
-    textRect = textObj.get_rect()
-    textRect.topleft = (x, y)
-    surface.blit(textObj, textRect)
+bg_music = pg.mixer.music.load('musicas/BoxCat Games - Defeat.mp3')
+pg.mixer.music.play(-1)
+
+
+running = True
+while running:
+
+    screen.fill([0, 0, 0])
     
-class MainMenu:
-    def __init__(self):
-        running = True
-        while running:
-                
-            screen.fill([0, 0, 0])
-            draw_text('Main Menu', font, 1, (255, 255, 255), screen, 40, 30)
-            
-            clock.tick(30)
-            pg.display.update()
-            
-            for event in pg.event.get():
-                if event.type == QUIT:
-                    pg.quit()
-                    sys.exit()
+    menu1 = menu()
+    clock.tick(30)
+    pg.display.update()
 
-    # @classmethod
-    # def draw_text(self, text, font, type, color, surface, x, y):
-    #     textObj = font.render(text, type, color)
-    #     textRect = textObj.get_rect()
-    #     textRect.topleft = (x, y)
-    #     surface.blit(textObj, textRect)
-            
-# def main_menu():
-#     running = True
-#     while running:
-
-#         screen.fill([0, 0, 0])
-#         draw_text('Main Menu', font, 1, (255, 255, 255), screen, 40, 30)
-        
-#         clock.tick(30)
-#         pg.display.update()
-        
-#         for event in pg.event.get():
-#             if event.type == QUIT:
-#                 pg.quit()
-#                 sys.exit()
-
-menu1 = MainMenu()
-menu1.draw_text('Main Menu', font, 1, (255, 255, 255), screen, 40, 30)
+    for event in pg.event.get():
+        if event.type == QUIT:
+            pg.quit()
+            sys.exit()
