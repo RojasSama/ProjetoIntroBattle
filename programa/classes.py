1a8c07460f645136f72c5042160b9bef25e38bfd
Luiz <luizfrojas@gmail.com>
2021-12-03 15:24:54 -0300

    adicionando um catalogo de sprites dos personagens

--- programa/classes.py
+++ programa/classes.py
@@ -1,10 +1,22 @@
+import pygame
+
+
 class Character():
+    catalog = {
+        'bruxa': pygame.transform.scale2x(pygame.image.load('Personagens/bruxa.png')),
+        'cacadora': pygame.transform.scale2x(pygame.image.load('Personagens/cacadora.png')),
+        'caveira': pygame.transform.scale2x(pygame.image.load('Personagens/caveira.png')),
+        'clerigo': [pygame.transform.scale2x(pygame.image.load('Personagens/clerigo(com_sombra.png)')), pygame.transform.scale2x(pygame.image.load('Personagens/clerigo(sem_sombra.png)'))],
+        'mago': pygame.transform.scale2x(pygame.image.load('Personagens/mago(final).png')),
+        'paladino': pygame.transform.scale2x(pygame.image.load('Personagens/paladino.png')),
+        'vampiro': pygame.transform.scale2x(pygame.image.load('Personagens/vampiro.png')),
+    }
     '''
     Atributes:
         hp : character health points,
         atk : character attack points,
         df : character defense points,
-        spd : character speed
+        spd : character speed,
     '''
     def __init__(self, hp, atk, df, spd, defeat=False):
         self.hp = hp
@@ -23,6 +35,9 @@ class Character():
     def verifies_defeat(self):
         if self.hp == 0:
             self.defense = True
+    
+    def blit_character(self, x, y, img):
+        pass
         
 
 class Battle(Character):
@@ -36,12 +51,11 @@ class Battle(Character):
         e3 : enemy 3
     '''
     def __init__(self, c1, c2, c3, e1, e2, e3):
-        Character.__init__(self, hp, atq, df, vl, defeat=False)
+        # Character.__init__(self, hp, atq, df, vl, defeat=False)
         self.c1 = c1
         self.c2 = c2
         self.c3 = c3
         self.e1 = e1
         self.e2 = e2
         self.e3 = e3
-
     
