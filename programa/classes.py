dcec639c63d367eedb2a8eea0f3ad84c7d98df01
Luiz <luizfrojas@gmail.com>
2021-12-05 22:30:02 -0300

    pequenos detalhes add a classe Battle e Character

--- programa/classes.py
+++ programa/classes.py
@@ -2,7 +2,16 @@ import pygame
 
 
 class Character():
-    '''
+    catalog = {
+        'bruxa': pygame.transform.scale2x(pygame.image.load('Personagens/bruxa.png')),
+        'cacadora': pygame.transform.scale2x(pygame.image.load('Personagens/cacadora.png')),
+        'caveira': pygame.transform.scale2x(pygame.image.load('Personagens/caveira.png')),
+        'clerigo': pygame.transform.scale2x(pygame.image.load('Personagens/clerigo(sem_sombra).png')),
+        'mago': pygame.transform.scale2x(pygame.image.load('Personagens/mago(final).png')),
+        'paladino': pygame.transform.scale2x(pygame.image.load('Personagens/paladino.png')),
+        'vampiro': pygame.transform.scale2x(pygame.image.load('Personagens/vampiro.png'))
+    }
+    '''    
     Atributes:
         hp : character health points,
         atk : character attack points,
@@ -16,15 +25,6 @@ class Character():
         self.spd = spd
         self.defense = defeat
         
-        self.catalog = {
-            'bruxa': pygame.transform.scale2x(pygame.image.load('Personagens/bruxa.png')),
-            'cacadora': pygame.transform.scale2x(pygame.image.load('Personagens/cacadora.png')),
-            'caveira': pygame.transform.scale2x(pygame.image.load('Personagens/caveira.png')),
-            'clerigo': [pygame.transform.scale2x(pygame.image.load('Personagens/clerigo(com_sombra.png)')), pygame.transform.scale2x(pygame.image.load('Personagens/clerigo(sem_sombra.png)'))],
-            'mago': pygame.transform.scale2x(pygame.image.load('Personagens/mago(final).png')),
-            'paladino': pygame.transform.scale2x(pygame.image.load('Personagens/paladino.png')),
-            'vampiro': pygame.transform.scale2x(pygame.image.load('Personagens/vampiro.png'))
-        }
 
     def attack(self, inimigo):
         damage = (self.atk * 50) // 50 + (inimigo.df * 2)
@@ -37,8 +37,8 @@ class Character():
         if self.hp == 0:
             self.defense = True
     
-    def blit_character(self, x, y, img):
-        pass
+    def blit_character(self, x, y, img, screen):
+        screen.blit(img, (x, y))
         
 
 class Battle(Character):
@@ -52,11 +52,14 @@ class Battle(Character):
         e3 : enemy 3
     '''
     def __init__(self, c1, c2, c3, e1, e2, e3):
-        # Character.__init__(self, hp, atq, df, vl, defeat=False)
+        Character.__init__(c1, c2, c3, e1, defeat=False)
         self.c1 = c1
         self.c2 = c2
         self.c3 = c3
         self.e1 = e1
         self.e2 = e2
         self.e3 = e3
+    
+    def blit_characters(self):
+        pass
     
