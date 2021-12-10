4593ba2df3e1df9ffca268d77eb51ccf63fcc66f
Luiz <luizfrojas@gmail.com>
2021-12-09 22:19:22 -0300

    redimensionando as imagens no menu, restando apenas o posicionamento correto, e exibir o cursor

--- programa/character.py
+++ programa/character.py
@@ -14,7 +14,7 @@ class Character():
         self.defense = 10
         self.speed = 2
         self.defeat = False
-        self.ui_bg = pg.transform.scale(pg.image.load('UI/introcomp_menu(resized).png'), (300, 300))
+        self.ui_bg = pg.transform.scale(pg.image.load('UI/introcomp_menu(resized).png'), (150, 150))
         self.shadow = pg.image.load('UI/introcomp_character.png')
         self.catalog = {  # catalogo de sprites dos personagens
             'wizard': pg.transform.scale(pg.image.load('Personagens/mago(final).png'), (90, 90)),
