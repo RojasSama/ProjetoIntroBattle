a9ade29ed40e3c36e8b8da5a073eca545b6c075a
Luiz <luizfrojas@gmail.com>
2021-12-16 21:00:08 -0300

    criando uma branch para organizar um exposicao

--- programa/battle_system.py
+++ programa/battle_system.py
@@ -2,18 +2,18 @@ import pygame as pg
 from game import *
 from character import *
 
-class Battle():
+class Battle(SelectMenu):
     def __init__(self, game):
+        SelectMenu.__init__(self, game)
         self.game = game
         self.running, self.playing = False, False
         self.bg_img = pg.transform.scale(pg.image.load('Background/cenario(lutas).png'), (1024, 768))
         self.ui_1 = pg.transform.scale(pg.image.load('UI/introcomp_menu(resized).png'), (650, 250))
         self.ui_2 = pg.transform.scale(self.ui_1, (360, 250))
         self.cursor = pg.transform.scale(pg.image.load('UI/introcomp_seta(resized).png'), (10, 10))
-        # self.Character
         self.turn = 'Player'
         self.position = 'Attack'
-        self.coord_enemies = {'witch': [850, 75], 'skeleton': [850, 450]}
+        self.coord_enemies = {'witch': [850, 185, pg.transform.flip(SelectMenu(self.game).char.catalog['witch'], True, False)], 'skeleton': [850, 350, pg.transform.flip(SelectMenu(self.game).char.catalog['skeleton'], True, False)]}
 
     def show_hp(self):
         # total_hp_team = [self.player_team[0].health, self.player_team[1].health, self.player_team[2].health]
@@ -22,7 +22,7 @@ class Battle():
         self.game.draw_text(f'{Rogue.__name__} - {12} / {100}', 35, 850, 550, self.game.BLACK)
         self.game.draw_text(f'{Wizard.__name__} - {50} / {100}', 35, 850, 600, self.game.BLACK)
         self.game.draw_text(f'{Hunter.__name__} - {75} / {100}', 35, 850, 650, self.game.BLACK)
-    
+
     def display_scenery(self):
         
         self.running = True
@@ -36,18 +36,22 @@ class Battle():
             if self.turn == 'Player':
                 self.game.draw_text("Player's turn!", 40, 175, 560, self.game.BLACK)
                 self.game.draw_text('Attack', 35, 175, 650, self.game.BLACK)
-                self.game.draw_text('Defensor', 35, 390, 650, self.game.BLACK)
+                self.game.draw_text('Defend', 35, 390, 650, self.game.BLACK)
 
                 self.show_hp()
 
-            ########################################### Apresentando problemas ################################################################################
+                Character().blit_character(250, 185)
+                Character().blit_character()
+                Character().blit_character()
+
+            ########################################### Apresentando problemas #######################
             # self.player_team[0].blit_character(250, 75, self.player_team[0].img, self.game.display)
             # self.player_team[1].blit_character(50, 250, self.player_team[1].img, self.game.display)
             # self.player_team[2].blit_character(250, 450, self.player_team[3].img, self.game.display)
-
-            Character.blit_character(self.coord_enemies['witch'][0], self.coord_enemies['witch'][1], 75, self.game.char.catalog['witch'], self.game.display)
-            # Character.blit_character(self.coord_enemies['Skeleton'][0], self.coord_enemies['Skeleton'][1], Character.catalog['skeleton'], self.game.display)
-            ####################################################################################################################################################
+            ##########################################################################################
+            
+            Character().blit_character(self.coord_enemies['witch'][0], self.coord_enemies['witch'][1], self.coord_enemies['witch'][2], self.game.display)
+            Character().blit_character(self.coord_enemies['skeleton'][0], self.coord_enemies['skeleton'][1], self.coord_enemies['skeleton'][2], self.game.display)
 
             self.game.main_menu.blit_screen()
     
