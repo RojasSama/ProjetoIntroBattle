72c232902501bae725eae058ca7b7f7a65831dbf
Luiz <luizfrojas@gmail.com>
2021-12-16 23:10:18 -0300

    finalizando a parte de exibicao, com imagens meramente ilustrativas do funcionamento do game

--- programa/battle_system.py
+++ programa/battle_system.py
@@ -10,10 +10,26 @@ class Battle(SelectMenu):
         self.bg_img = pg.transform.scale(pg.image.load('Background/cenario(lutas).png'), (1024, 768))
         self.ui_1 = pg.transform.scale(pg.image.load('UI/introcomp_menu(resized).png'), (650, 250))
         self.ui_2 = pg.transform.scale(self.ui_1, (360, 250))
-        self.cursor = pg.transform.scale(pg.image.load('UI/introcomp_seta(resized).png'), (10, 10))
+        self.cursor = pg.transform.scale(pg.image.load('UI/introcomp_seta(resized).png'), (25, 25))
         self.turn = 'Player'
         self.position = 'Attack'
         self.coord_enemies = {'witch': [850, 185, pg.transform.flip(SelectMenu(self.game).char.catalog['witch'], True, False)], 'skeleton': [850, 350, pg.transform.flip(SelectMenu(self.game).char.catalog['skeleton'], True, False)]}
+    
+    def display_cursor(self, position):
+        self.positions = {'attack' : [45, 700], 'defend' : [260, 700]}
+        '''if self.position == 'Attack':
+            self.game.display.blit((pg.transform.rotate(self.cursor, -90)), (155, 560))
+        
+        elif self.position == 'Defend':
+            self.game.display.blit((pg.transform.rotate(self.cursor, -90)) , (380, 650))
+        
+        elif self.position == 'Choosing enemy':
+            self.game.display.blit(self.cursor, (850, 160))'''
+        
+        self.imgx, self.imgy = self.positions[position][0] + 50, self.positions[position][1] - 50  # as coordenadas do cursor sao baseadas nas dos personagens
+        img_rect = self.cursor.get_rect()
+        img_rect.center = (self.imgx, self.imgy)  # coordenadas do cursor
+        self.game.display.blit(pg.transform.rotate(self.cursor, 90), img_rect)
 
     def show_hp(self):
         # total_hp_team = [self.player_team[0].health, self.player_team[1].health, self.player_team[2].health]
@@ -39,10 +55,11 @@ class Battle(SelectMenu):
                 self.game.draw_text('Defend', 35, 390, 650, self.game.BLACK)
 
                 self.show_hp()
+                self.display_cursor('attack')
 
-                Character().blit_character(250, 185)
-                Character().blit_character()
-                Character().blit_character()
+                Character().blit_character(50, 185, SelectMenu(self.game).char.catalog['rogue'], self.game.display)
+                Character().blit_character(90, 255, SelectMenu(self.game).char.catalog['wizard'], self.game.display)
+                Character().blit_character(50, 350, SelectMenu(self.game).char.catalog['hunter'], self.game.display)
 
             ########################################### Apresentando problemas #######################
             # self.player_team[0].blit_character(250, 75, self.player_team[0].img, self.game.display)
@@ -54,9 +71,6 @@ class Battle(SelectMenu):
             Character().blit_character(self.coord_enemies['skeleton'][0], self.coord_enemies['skeleton'][1], self.coord_enemies['skeleton'][2], self.game.display)
 
             self.game.main_menu.blit_screen()
-    
-    def draw_cursor(self):
-        pass
 
 class Turn(Battle):
     def __init__(self):
